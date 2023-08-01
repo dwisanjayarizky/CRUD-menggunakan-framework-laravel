e80038a1fbc907d14cc21b9b82590cf074e2ced4
dwisanjayarizky <dwisanjayarizki@gmail.com>
2023-08-01 14:42:16 +0700

    UAS_pemweb

---
+++ database/migrations/2023_08_01_043647_semester.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semester', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_semester')->nullable();
+            $table->string('semester')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('semester');
+    }
+};
---
+++ database/migrations/2023_08_01_043803_matakuliah.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matakuliah', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode matakuliah')->nullable();
+            $table->string('nama matakuliah')->nullable();
+            $table->string('sesi matakuliah')->nullable();
+            $table->string('jumlah ruangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matakuliah');
+    }
+};
